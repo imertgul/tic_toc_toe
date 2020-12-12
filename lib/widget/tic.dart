8d19966576fc52d8971bde985c25de4b8e402e8c
Mert Gül <ibrahimmertgul@gmail.com>
2020-12-12 15:05:26 +0300

    tic plays

--- lib/widget/tic.dart
+++ lib/widget/tic.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import '../model/Game.dart';
 
 typedef OnChanged = void Function(String);
 
 class Tic extends StatefulWidget {
-  OnChanged onChanged;
-  Tic({this.onChanged});
+  int x;
+  final bool isPlayed;
+  final OnChanged onChanged;
+  Tic({this.onChanged, this.isPlayed = false, this.x});
   @override
   _TicState createState() => _TicState();
 }
 
 class _TicState extends State<Tic> {
-  String value = "X";
+  String value = " ";
+  pressed() {
+    setState(() {
+      if (!widget.isPlayed) {
+        myGame.turn == 0 ? value = "X" : value = "O";
+      }
+      myGame.map[widget.x] = value;
+    });
+    widget.onChanged("Oynandı");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text(value);
+    return TextButton(
+      onPressed: () => pressed(),
+      child: Text(value),
+    );
+    // return Text(value);
   }
 }
